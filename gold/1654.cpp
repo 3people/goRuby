f9929138ece31042f330abdc7d043b65c8b3072d
3people <smsn7031@naver.com>
2020-05-23 16:20:07 +0900

    5/23

--- gold/1654.cpp
+++ gold/1654.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+typedef long long ll;
+using namespace std;
 
+ll search(vector<ll> v, ll start, ll end, int n)
+{
+    ll mid = (start + end) / 2;
+    ll sum = 0;
+    while (start <= end)
+    {
+        sum = 0;
+        mid = (start + end) / 2;
+        for (int i = 0; i < v.size(); i++)
+        {
+            sum += v[i] / mid;
+        }
+        if (n > sum)
+        {
+            end = mid - 1;
+        }
+        else if (n <= sum)
+        {
+            start = mid + 1;
+        }
+    }
+    return end;
+}
+int main()
+{
+    int k, n;
+    cin >> k >> n;
+    vector<ll> v;
+    for (int i = 0; i < k; i++)
+    {
+        ll lan;
+        cin >> lan;
+        v.push_back(lan);
+    }
+    sort(v.begin(), v.end());
+    cout << search(v, 1, v[v.size() - 1], n);
+    return 0;
+}

169273d4b13691e4a845a9ea206ac22e5cfb1d3a
3people <smsn7031@naver.com>
2020-06-20 12:59:32 +0900

    6/20

---
+++ gold/8371.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int n, ans = 0;
+    cin >> n;
+    string before, after;
+    cin >> before >> after;
+    for (int i = 0; i < n; i++)
+    {
+        if (before[i] != after[i])
+            ans++;
+    }
+    cout << ans;
+}

4191c742192ae79ec8a4394c7573b5738061a83c
3people <smsn7031@naver.com>
2020-06-28 17:50:42 +0900

    6/28

--- gold/2846.cpp
+++ gold/2846.cpp
@@ -1,22 +1,28 @@
-#include <stdio.h>
-
+#include <iostream>
+using namespace std;
 int main()
 {
-    int n;
+    int n, cnt = 0, ans = 0;
     int arr[1001];
-    int max = 0, count = 0;
-
-    scanf("%d", &n);
-
+    cin >> n;
     for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
+    {
+        cin >> arr[i];
+    }
     for (int i = 1; i < n; i++)
     {
-        arr[i - 1] < arr[i] ? count += arr[i] - arr[i - 1] : count = 0;
-
-        if (max < count)
-            max = count;
+        if (arr[i - 1] < arr[i])
+        {
+            cnt += arr[i] - arr[i - 1];
+        }
+        else
+        {
+            cnt = 0;
+        }
+        if (ans < cnt)
+        {
+            ans = cnt;
+        }
     }
-    printf("%d", max);
+    cout << ans;
 }

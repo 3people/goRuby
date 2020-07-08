6c8a615865cb4e472a3c6ac76b1b4a938a36d9db
3people <smsn7031@naver.com>
2020-07-08 20:56:44 +0900

    7/8

---
+++ gold/2445.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = n * 2 - (2 * (i + 1)); j > 0; j--)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = n - 1; j > i; j--)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < (i + 1) * 2; j++)
+        {
+            cout << " ";
+        }
+        for (int j = n - 1; j > i; j--)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}

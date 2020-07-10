3dab6dc95b2d85a9e6eea48e7c65111384bd4598
3people <smsn7031@gmail.com>
2020-07-10 10:37:19 +0900

    7/10

---
+++ gold/17614.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int num, cnt = 0;
+    cin >> num;
+    for (int i = 1; i <= num; i++)
+    {
+        string str = to_string(i);
+        for (int j = 0; j < str.size(); j++)
+        {
+            if (str[j] == '3' || str[j] == '6' || str[j] == '9')
+            {
+                cnt++;
+            }
+        }
+    }
+    cout << cnt;
+}

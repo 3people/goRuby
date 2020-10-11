f789b11bc78bac065904b6db905dacc39233465d
3people <smsn7031@naver.com>
2020-10-11 21:56:24 +0900

    10/11

---
+++ gold/15654.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n,m;
+int arr[9]={0,};
+int visited[9]={0,};
+vector<int> v;
+
+void dfs(int cnt){
+    if (cnt == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (visited[i])
+            continue;
+        visited[i] = true;
+        arr[cnt] = v[i];
+        dfs(cnt+1);
+        visited[i] =false;
+    }
+    
+}
+int main() {
+    cin >> n >> m;
+    v.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(),v.end());
+    dfs(0);
+}
